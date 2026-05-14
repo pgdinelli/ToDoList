e0cb362c9f55acf7db04335d8ae44b43d768b751
dinellipaulo <pgsd1453@gmail.com>
2026-05-14 16:06:18 -0400

    removendo divs desnecessárias do layout

--- frontend/src/components/TaskForm/index.jsx
+++ frontend/src/components/TaskForm/index.jsx
@@ -11,46 +11,43 @@ function TaskForm() {
                     <button className="bg-green-400 px-1 cursor-pointer rounded-sm">Adicionar</button>
                 </form>
             </div>
+            
+            <ul className="w-full flex flex-col gap-3">
+                <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
+                    <div className="flex items-center gap-2">
+                        <button className="cursor-pointer">
+                            <FaRegSquare />
+                        </button>
+                        Tarefa de teste 1
+                    </div>
+                    <div className="flex items-center gap-3 text-2xl">
+                        <button className="cursor-pointer">
+                            <FaEdit className="text-amber-600" />
+                        </button>
+                        <button className="cursor-pointer">
+                            <FaTrash className="text-red-600" />
+                        </button>
+                    </div>
+                </li>
 
-            <div className="bg-gray-300 w-full p-2 rounded-md">
-                <ul className="w-full flex flex-col gap-3">
-                    <li className="w-full flex items-center justify-between gap-2">
-                        <div className="flex items-center gap-2">
-                            <button className="cursor-pointer">
-                                <FaRegSquare />
-                            </button>
-                            Tarefa de teste 1
-                        </div>
-                        <div className="flex items-center gap-3 text-2xl">
-                            <button className="cursor-pointer">
-                                <FaEdit className="text-amber-600" />
-                            </button>
-                            <button className="cursor-pointer">
-                                <FaTrash className="text-red-600" />
-                            </button>
-                        </div>
-                    </li>
+                <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
+                    <div className="flex items-center gap-2">
+                        <button className="cursor-pointer">
+                            <FaRegCheckSquare />
+                        </button>
+                        Tarefa de teste 2
+                    </div>
+                    <div className="flex items-center gap-3 text-2xl">
+                        <button className="cursor-pointer">
+                            <FaEdit className="text-amber-600" />
+                        </button>
+                        <button className="cursor-pointer">
+                            <FaTrash className="text-red-600" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
 
-                    <hr />
-
-                    <li className="w-full flex items-center justify-between gap-2">
-                        <div className="flex items-center gap-2">
-                            <button className="cursor-pointer">
-                                <FaRegCheckSquare />
-                            </button>
-                            Tarefa de teste 2
-                        </div>
-                        <div className="flex items-center gap-3 text-2xl">
-                            <button className="cursor-pointer">
-                                <FaEdit className="text-amber-600" />
-                            </button>
-                            <button className="cursor-pointer">
-                                <FaTrash className="text-red-600" />
-                            </button>
-                        </div>
-                    </li>
-                </ul>
-            </div>
         </div>
     )
 }
