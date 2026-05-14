5671f654071510dd762387020790c70a6f29091f
dinellipaulo <pgsd1453@gmail.com>
2026-05-14 14:38:05 -0400

    Form de tarefas

--- frontend/src/components/TaskForm/index.jsx
+++ frontend/src/components/TaskForm/index.jsx
@@ -1,7 +1,14 @@
 function TaskForm() {
     return (
         <div className="h-fit w-1/2 p-5 bg-white rounded-xl">
-            <h1>Este é o formulário de tasks</h1>
+            <div className="w-full">
+                <form className="flex justify-between gap-4">
+                    <input type="text" placeholder="Adicione uma tarefa..." 
+                    className="bg-gray-300 w-full py-1 px-2 rounded-md outline-0"
+                    />
+                    <button className="bg-green-400 px-1 cursor-pointer rounded-sm">Adicionar</button>
+                </form>
+            </div>
         </div>
     )
 }
