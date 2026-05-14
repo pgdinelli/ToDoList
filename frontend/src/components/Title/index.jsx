f25e4b1468eece634452e8cbde6d71ee937cb814
dinellipaulo <pgsd1453@gmail.com>
2026-05-14 14:07:03 -0400

    Esqueleto inicial do frontend v2

--- frontend/src/components/Title/index.jsx
+++ frontend/src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 function Title() {
     return (
-        <div className="h-fit w-1/2 p-5 text-center rounded-xl">
+        <div className="h-fit w-1/2 p-5 text-center">
             <h1 className="text-3xl mb-1">Lista de Tarefas</h1>
             <p>Adicione tarefas abaixo</p>
         </div>
