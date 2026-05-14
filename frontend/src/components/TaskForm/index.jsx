61c47455949f2fa4afdd0190110952d1569107ee
dinellipaulo <pgsd1453@gmail.com>
2026-05-14 15:23:30 -0400

    Adicionando tarefas de teste

--- frontend/src/components/TaskForm/index.jsx
+++ frontend/src/components/TaskForm/index.jsx
@@ -1,29 +1,51 @@
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaEdit, FaTrash, FaRegSquare, FaRegCheckSquare } from "react-icons/fa";
 
 function TaskForm() {
     return (
         <div className="h-fit w-1/2 p-5 bg-white rounded-xl">
             <div className="w-full mb-3.5">
                 <form className="flex justify-between gap-4">
-                    <input type="text" placeholder="Adicione uma tarefa..." 
-                    className="bg-gray-300 w-full py-1 px-2 rounded-md outline-0"
+                    <input type="text" placeholder="Adicione uma tarefa..."
+                        className="bg-gray-300 w-full py-1 px-2 rounded-md outline-0"
                     />
                     <button className="bg-green-400 px-1 cursor-pointer rounded-sm">Adicionar</button>
                 </form>
             </div>
 
-            <div className="bg-gray-300 w-full p-2 flex justify-between items-center rounded-md">
-                <ul>
-                    <li>Tarefa de teste 1</li>
+            <div className="bg-gray-300 w-full p-2 rounded-md">
+                <ul className="w-full flex flex-col gap-3">
+                    <li className="w-full flex items-center justify-between gap-2">
+                        <div className="flex items-center gap-2">
+                            <FaRegSquare />
+                            Tarefa de teste 1
+                        </div>
+                        <div className="flex items-center gap-3 text-2xl">
+                            <button className="cursor-pointer">
+                                <FaEdit className="text-amber-600" />
+                            </button>
+                            <button className="cursor-pointer">
+                                <FaTrash className="text-red-600" />
+                            </button>
+                        </div>
+                    </li>
+
+                    <hr />
+
+                    <li className="w-full flex items-center justify-between gap-2">
+                        <div className="flex items-center gap-2">
+                            <FaRegCheckSquare />
+                            Tarefa de teste 2
+                        </div>
+                        <div className="flex items-center gap-3 text-2xl">
+                            <button className="cursor-pointer">
+                                <FaEdit className="text-amber-600" />
+                            </button>
+                            <button className="cursor-pointer">
+                                <FaTrash className="text-red-600" />
+                            </button>
+                        </div>
+                    </li>
                 </ul>
-                <div className="flex gap-3 items-center text-2xl">
-                    <button className="cursor-pointer">
-                        <FaEdit className="text-amber-600"/>
-                    </button>
-                    <button className="cursor-pointer">
-                        <FaTrash className="text-red-600"/>
-                    </button>
-                </div>
             </div>
         </div>
     )
