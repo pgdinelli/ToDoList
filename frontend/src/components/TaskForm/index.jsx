d57879606e6d6d622e9e6f7be77be9918b5c4a8a
dinellipaulo <pgsd1453@gmail.com>
2026-05-15 13:03:33 -0400

    renderizando tarefas salvas na tela

--- frontend/src/components/TaskForm/index.jsx
+++ frontend/src/components/TaskForm/index.jsx
@@ -41,44 +41,32 @@ function TaskForm() {
                 </form>
             </div>
 
-            <ul>
-                <div className="flex flex-col">
-                    <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
-                        <div className="flex items-center gap-2">
-                            <button className="cursor-pointer">
-                                <FaRegSquare />
-                            </button>
-                            <h2>Tarefa de teste 1</h2>
-                        </div>
+            <ul className="flex flex-col gap-2">
+                {tasksArr.map((task) => {
+                    return (
+                        <>
+                        <li key={task._id} className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
+                            <div className="flex items-center gap-2">
+                                <button className="cursor-pointer">
+                                    <FaRegSquare />
+                                </button>
+                                <h2>{task.title}</h2>
+                            </div>
 
-                        <div className="flex items-center gap-3 text-2xl">
-                            <button className="cursor-pointer">
-                                <FaEdit className="text-amber-600" />
-                            </button>
-                            <button className="cursor-pointer">
-                                <FaTrash className="text-red-600" />
-                            </button>
-                        </div>
-                    </li>
-                    <p className="p-2 text-gray-500 italic">Breve descrição da tarefa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iure porro sed maxime tempora, dolorum, praesentium quia eaque laborum, facilis tenetur soluta cum reiciendis. Quisquam dicta officiis dignissimos debitis maxime dolorem adipisci dolor eos nobis. Ea nostrum modi architecto. Quibusdam, corporis beatae. Dolore possimus dolor quis enim eos, accusantium eum veniam tempora fuga rem voluptate eaque! Deserunt magnam temporibus veniam dolor vitae et illum consectetur sint. Quis repudiandae vitae exercitationem aliquid esse nobis nisi odio at modi porro temporibus facere est, iure magni, voluptate voluptatem id! Ex, obcaecati. Mollitia autem aperiam numquam delectus consectetur corporis maxime voluptatem tempore consequuntur error.</p>
-                </div>
-
-                <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
-                    <div className="flex items-center gap-2">
-                        <button className="cursor-pointer">
-                            <FaRegCheckSquare />
-                        </button>
-                        Tarefa de teste 2
-                    </div>
-                    <div className="flex items-center gap-3 text-2xl">
-                        <button className="cursor-pointer">
-                            <FaEdit className="text-amber-600" />
-                        </button>
-                        <button className="cursor-pointer">
-                            <FaTrash className="text-red-600" />
-                        </button>
-                    </div>
-                </li>
+                            <div className="flex items-center gap-3 text-2xl">
+                                <button className="cursor-pointer">
+                                    <FaEdit className="text-amber-600" />
+                                </button>
+                                <button className="cursor-pointer">
+                                    <FaTrash className="text-red-600" />
+                                </button>
+                            </div>
+                            
+                        </li>
+                        <p className="p-1 text-gray-500 italic">{task.description}</p>
+                        </>
+                    )
+                })}
             </ul>
 
         </div>
