e89871ae87222dac84b5a1caefa43b62f7b66077
dinellipaulo <pgsd1453@gmail.com>
2026-05-15 11:19:57 -0400

    Adicionando campos de descrição da tarefa

--- frontend/src/components/TaskForm/index.jsx
+++ frontend/src/components/TaskForm/index.jsx
@@ -8,27 +8,34 @@ function TaskForm() {
                     <input type="text" placeholder="Adicione uma tarefa..."
                         className="bg-gray-300 w-full py-1 px-2 rounded-md outline-0"
                     />
+                    <textarea name="description" id="description" 
+                    placeholder="Insira uma breve descrição"
+                    className="py-1 px-2 border outline-0"></textarea>
                     <button className="bg-green-400 px-1 cursor-pointer rounded-sm">Adicionar</button>
                 </form>
             </div>
-            
-            <ul className="w-full flex flex-col gap-3">
-                <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
-                    <div className="flex items-center gap-2">
-                        <button className="cursor-pointer">
-                            <FaRegSquare />
-                        </button>
-                        Tarefa de teste 1
-                    </div>
-                    <div className="flex items-center gap-3 text-2xl">
-                        <button className="cursor-pointer">
-                            <FaEdit className="text-amber-600" />
-                        </button>
-                        <button className="cursor-pointer">
-                            <FaTrash className="text-red-600" />
-                        </button>
-                    </div>
-                </li>
+
+            <ul>
+                <div className="flex flex-col">
+                    <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
+                        <div className="flex items-center gap-2">
+                            <button className="cursor-pointer">
+                                <FaRegSquare />
+                            </button>
+                            <h2>Tarefa de teste 1</h2>
+                        </div>
+                    
+                        <div className="flex items-center gap-3 text-2xl">
+                            <button className="cursor-pointer">
+                                <FaEdit className="text-amber-600" />
+                            </button>
+                            <button className="cursor-pointer">
+                                <FaTrash className="text-red-600" />
+                            </button>
+                        </div>
+                    </li>
+                    <p className="p-2 text-gray-500 italic">Breve descrição da tarefa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iure porro sed maxime tempora, dolorum, praesentium quia eaque laborum, facilis tenetur soluta cum reiciendis. Quisquam dicta officiis dignissimos debitis maxime dolorem adipisci dolor eos nobis. Ea nostrum modi architecto. Quibusdam, corporis beatae. Dolore possimus dolor quis enim eos, accusantium eum veniam tempora fuga rem voluptate eaque! Deserunt magnam temporibus veniam dolor vitae et illum consectetur sint. Quis repudiandae vitae exercitationem aliquid esse nobis nisi odio at modi porro temporibus facere est, iure magni, voluptate voluptatem id! Ex, obcaecati. Mollitia autem aperiam numquam delectus consectetur corporis maxime voluptatem tempore consequuntur error.</p>
+                </div>
 
                 <li className="w-full flex items-center justify-between gap-2 bg-gray-300 p-2 rounded-md">
                     <div className="flex items-center gap-2">
